64580cadb702a9b53112f7b31f24bc6b4a2c3b04
tomochinn <122351367+tomochinn@users.noreply.github.com>
2023-06-25 05:46:29 +0000

    退会後のアラート実装

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -3,9 +3,14 @@ class CartItem < ApplicationRecord
   belongs_to :customer
   belongs_to :item
 
-  ## 小計を求めるメソッド
+  # 小計を求めるメソッド
   def subtotal
     item.with_tax_price * amount
+  end
+  
+  # カートが空か確認するメソッド
+  def empty?
+    cart_items.empty?
   end
 
 end
